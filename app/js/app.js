e8938cf0eaa68d0d9d20fea2a89eb84c7e6dc38b
Feldmar <89993722+Feldmar@users.noreply.github.com>
2021-11-25 20:25:10 +0300

    re re re re

--- app/js/app.js
+++ app/js/app.js
@@ -2,37 +2,38 @@
 
 // require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
 
-function scrollTo(to, duration = 700) {
-    const
-        element = document.scrollingElement || document.documentElement,
-        start = element.scrollTop,
-        change = to - start,
-        startDate = +new Date(),
-        // t = current time
-        // b = start value
-        // c = change in value
-        // d = duration
-        easeInOutQuad = function (t, b, c, d) {
-            t /= d / 2;
-            if (t < 1) return c / 2 * t * t + b;
-            t--;
-            return -c / 2 * (t * (t - 2) - 1) + b;
-        },
-        animateScroll = function () {
-            const currentDate = +new Date();
-            const currentTime = currentDate - startDate;
-            element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
-            if (currentTime < duration) {
-                requestAnimationFrame(animateScroll);
-            }
-            else {
-                element.scrollTop = to;
-            }
-        };
-    animateScroll();
-}
+
 
 document.addEventListener('DOMContentLoaded', function () {
+    function scrollTo(to, duration = 700) {
+        const
+            element = document.scrollingElement || document.documentElement,
+            start = element.scrollTop,
+            change = to - start,
+            startDate = +new Date(),
+            // t = current time
+            // b = start value
+            // c = change in value
+            // d = duration
+            easeInOutQuad = function (t, b, c, d) {
+                t /= d / 2;
+                if (t < 1) return c / 2 * t * t + b;
+                t--;
+                return -c / 2 * (t * (t - 2) - 1) + b;
+            },
+            animateScroll = function () {
+                const currentDate = +new Date();
+                const currentTime = currentDate - startDate;
+                element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
+                if (currentTime < duration) {
+                    requestAnimationFrame(animateScroll);
+                }
+                else {
+                    element.scrollTop = to;
+                }
+            };
+        animateScroll();
+    }
     let btn = document.querySelector('#toTop');
     window.addEventListener('scroll', function () {
         // Если прокрутили дальше 599px, показываем кнопку
